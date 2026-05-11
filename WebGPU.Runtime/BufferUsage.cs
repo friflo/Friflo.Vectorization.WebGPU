02e0b1ad2a260f39465f0f7bf8e8344b16654151
friflo <ullrich@praetz.de>
2026-05-11 12:18:50 +0200

    add enum BufferUsage

---
+++ WebGPU.Runtime/BufferUsage.cs
@@ -0,0 +1,18 @@
+﻿namespace Friflo.Vectorization.WebGPU.Runtime;
+
+using static WebGPU_native;
+
+public enum BufferUsage : ulong
+{
+    None            = BufferUsage_None,
+    MapRead         = BufferUsage_MapRead,
+    MapWrite        = BufferUsage_MapWrite,
+    CopySrc         = BufferUsage_CopySrc,
+    CopyDst         = BufferUsage_CopyDst,
+    Index           = BufferUsage_Index,
+    Vertex          = BufferUsage_Vertex,
+    Uniform         = BufferUsage_Uniform,
+    Storage         = BufferUsage_Storage,
+    Indirect        = BufferUsage_Indirect,
+    QueryResolve    = BufferUsage_QueryResolve
+}
